08f0b531e3223594a48272adcd0bb2cd48372776
datashowcloud <102643136+datashowcloud@users.noreply.github.com>
2022-09-25 17:45:05 -0300

    Add files via upload

---
+++ temp/p01.js
@@ -0,0 +1,6 @@
+var test = '<div id=\"div01\" style=\"display:\'none\';\">';
+test += '<button class=\"btn btn-info\" onclick=\"showPrevious(\'01\');\"> <i class=\"fa fa-backward\"></i></button>';
+test += '</div>';
+alert(test);
+document.body.innerHTML = test;
+//$("#result").text(test);
