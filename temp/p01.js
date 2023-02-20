e1ac3090fcbe5c027b299fc5bce984d8266562d8
datashowcloud <102643136+datashowcloud@users.noreply.github.com>
2023-02-20 14:35:36 -0300

    Delete temp directory

--- temp/p01.js
+++
@@ -1,6 +0,0 @@
-var test = '<div id=\"div01\" style=\"display:\'none\';\">';
-test += '<button class=\"btn btn-info\" onclick=\"showPrevious(\'01\');\"> <i class=\"fa fa-backward\"></i></button>';
-test += '</div>';
-alert(test);
-document.body.innerHTML = test;
-//$("#result").text(test);
